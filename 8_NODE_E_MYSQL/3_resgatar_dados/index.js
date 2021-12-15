033ff24061cbb393b94b9bc75d0fac23f58aebca
Leandro Bernardo <leandro.bernardos@gmail.com>
2021-12-14 23:42:34 -0300

    integracao node + mysql 1

---
+++ 8_NODE_E_MYSQL/3_resgatar_dados/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+//Configure express to get body
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+    const { title, pageqty } = req.body;
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`
+
+    conn.query(sql, (err) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        res.redirect('/books')
+    })
+})
+
+app.get('/books', (req, res) => {
+    const sql = `SELECT * FROM books`
+
+    conn.query(sql, (err, data) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        const books = data
+        res.render('books', { books })
+    })
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Jesus#12!00',
+    database: 'nodemysql'
+})
+
+conn.connect((err) => {
+    if (err) {
+        console.log(err)
+    }
+
+    console.log(`Banco conectado`)
+    app.listen(3000)
+
+})
